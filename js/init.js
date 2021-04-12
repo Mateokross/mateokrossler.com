deb28ec3cb6cdf2731ee9e94aeec1ec616e76180
Mateokross <40373197+Mateokross@users.noreply.github.com>
2021-04-12 15:04:08 -0300

    Nuevo cv, spannabis, bravo y trama

--- js/init.js
+++ js/init.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
         edge: 'right'
     });
     $('.materialboxed').materialbox();
+    // $('#carousel-eventos').carousel({
+    //     dist: -130,
+    //     numVisible: 3
+    // });
     $('#carousel-agrorrhh').carousel({
         dist: -130,
         numVisible: 3
@@ -11,4 +15,44 @@ $(document).ready(function () {
         dist: -30,
         numVisible: 3
     });
+
+    new Splide( '#splide-eventos', {
+        type   : 'loop',
+        perPage: 1,
+        autoplay: true,
+        lazyLoad: 'sequential',
+        pagination: false,
+        padding: 0,
+    }).mount();
+
+    new Splide( '#splide-agrorrhh', {
+        type   : 'loop',
+        perPage: 1,
+        autoplay: true,
+        lazyLoad: 'sequential',
+        width:1500,
+        heightRatio: 0.3333,
+        perPage:3,
+        focus    : 'center',
+        trimSpace: false,
+        arrows: false,
+        pagination: false,
+        gap: '1em',
+        breakpoints: {
+            1700: {
+                width:900,
+                heightRatio: 1,
+                perPage: 1,
+                heightRatio: 1,
+                gap: '0px',
+                padding: 200,
+            },
+            820: {
+                perPage: 1,
+                heightRatio: 1,
+                gap: '0px',
+                padding: 35,
+            },
+        }
+    }).mount();
 });
