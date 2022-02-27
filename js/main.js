5e4442299103dd17547763962dade455558dee15
Mateokross <40373197+Mateokross@users.noreply.github.com>
2022-02-26 22:17:33 -0300

    Primer commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
-$(window).on("load", function () {
-  /* ==========================================================================
-   Preloader
-   ========================================================================== */
-  $(".preloader-circle").css("display", "none");
-  $(".preloader").fadeOut(400);
-  $("body").removeClass("loading");
+$(document).ready(function(){
 
-  /* ==========================================================================
-    Auto-updating year (footer)
-    ========================================================================== */
-  $("span.year").text(new Date().getFullYear());
+
+    $(".section-links button").click(function(){
+        //set animation time
+        var time = 400
+        //get selected section
+        var section = $(this).data('section')
+        //hide active section
+        $(".section-content.active").fadeOut(time).removeClass("active")
+        $(".section-links button.active").removeClass("active")
+        //show section selected
+        setTimeout(function(){
+            $("." + section).fadeIn(time).addClass("active")
+            $(this).addClass("active")
+        }, time);
+        
+
+    });
+
+
+
+
 });
