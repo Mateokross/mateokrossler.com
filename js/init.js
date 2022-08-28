537e4e1fbef36de10ea4eee742be39bed689fe55
Mateo Krössler <mateokrossler@gmail.com>
2022-08-28 11:34:57 +0200

    avances en index

--- js/init.js
+++ js/init.js
@@ -1,17 +1,5 @@
 $(document).ready(function () {
 
-/* ==========================================================================
-  AOS - https://michalsnik.github.io/aos/
-  ========================================================================== */
-  // if(window.innerWidth >= 991){
-  //   AOS.init({
-  //       offset: 300,
-  //       delay: 200,
-  //       duration: 1800,
-  //       once: true
-  //     });
-  // }
-
   /* ==========================================================================
   Slider - https://splidejs.com/documents/
   ========================================================================== */
@@ -27,41 +15,25 @@ $(document).ready(function () {
       start: 0
     }).mount();
   }
-/* ==========================================================================
-  Materialize - https://materializecss.com/
-  ========================================================================== */
-    $('.sidenav').sidenav({
-        edge: 'right'
-    });
 
-
-/* ==========================================================================
-  TypewriterJS v2 - https://github.com/tameemsafi/typewriterjs
+  /* ==========================================================================
+  Tippy JS - https://atomiks.github.io/tippyjs/v6/getting-started/
   ========================================================================== */
 
-  //Si estoy en la página index, inicializo el typewriter
 
-  if($(".hero h4.spanish").length){
-    new Typewriter('.hero h4.spanish', {
-      strings: ['Lic. en Administración y Sistemas del ITBA', 'Analista de Producto en Kavak'],
-      autoStart: true,
-      cursor:"",
-      delay: 15,
-      deleteSpeed: 30,
-      loop: true,
-      pauseFor: 2500
-    });
-  
-    new Typewriter('.hero h4.english', {
-      strings: ['Business and Tech graduate from ITBA', 'Product Analyst at Kavak'],
-      autoStart: true,
-      cursor:"",
-      delay: 15,
-      deleteSpeed: 30,
-      loop: true,
-      pauseFor: 2500
-    });
-  }
+  const photographyModalContent = document.getElementById('photography-modal-content');
+
+  tippy('.tippy-photography',{
+    content: photographyModalContent.innerHTML,
+    allowHTML: true,
+    placement: 'bottom',
+    theme: 'mateo',
+    interactive: true,
+    maxWidth: 600,
+    offset: [0, 20],
+  });
+
+  console.log("test")
 
 
 
