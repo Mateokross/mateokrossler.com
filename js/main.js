fecd0d417ebf480a7a23129d5b233748a6f225b7
Mateo Krössler <mateokrossler@gmail.com>
2022-08-28 18:32:28 +0200

    fixes

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ $(window).on("load", function () {
     $(".project-selector .col.active").removeClass("active")
     $("#" + id).addClass("active")
     //hide active slider
-    $(".container.section.project.active").removeClass("active").addClass("hide");
+    $(".container.project.active").removeClass("active").addClass("hide");
     //show new slider
     var project = id.slice(10);
     $("#project-" + project).addClass("active").removeClass("hide")[0].scrollIntoView({behavior: "smooth", duration: 500});
