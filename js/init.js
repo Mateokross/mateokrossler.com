594266d6a4efe205c7c849b593d7bccdf593f6bb
Mateo Krössler <mateokrossler@gmail.com>
2022-12-09 22:58:53 -0300

    remove useless code, new text color, email tooltip

--- js/init.js
+++ js/init.js
@@ -21,6 +21,26 @@ $(document).ready(function () {
   ========================================================================== */
   //revisar setContent https://atomiks.github.io/tippyjs/v6/methods/
 
+  //Email tooltip for email links that don't work
+  // Use a counter variable to keep track of the number of times the element is clicked
+  let emailLinkClickCount = 0;
+  
+  $('.tippy-email').on('click', function() {
+    emailLinkClickCount++;
+    if (emailLinkClickCount === 2) {
+      const emailModal = tippy('.tippy-email.spanish',{
+        content: document.getElementById('email-modal-content').innerHTML,
+        placement: 'bottom',
+        allowHTML: true,
+        theme: 'mateo',
+        interactive: true,
+        popperOptions: {
+          strategy: 'fixed',
+        },
+      })[0].show();
+    }
+  });
+
 
   const developmentModalContentSpanish = document.getElementById('development-modal-content-spanish');
   const developmentModalContentEnglish = document.getElementById('development-modal-content-english');
